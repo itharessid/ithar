fc72150ed5ac4209208b74367b4d87b046d14593
ithar <ithar3333@gmail.com>
2024-02-12 11:40:21 +0100

    first

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Message } from "./Message";
+import ListGroup from "./components/listGroup";
+
+function App() {
+  return (
+    <ul className="list-group">
+      <li className="list-group-item"> 1</li>
+      <li className="list-group-item"> 1</li>{" "}
+    </ul>
+  );
+}
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,5 @@
+export function Message(){
+    //JSX / JS+XML 
+    const name ='ITHAR'
+    return <h1> hello {name} </h1>
+}
